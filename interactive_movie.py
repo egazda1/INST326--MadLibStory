90799d8c5189c1f153a5a338ce2779a9d88e4328
Emily Gazda <egazda1@terpmail.umd.edu>
2021-04-01 21:10:33 -0400

    added to interactive_movie.py

--- interactive_movie.py
+++ interactive_movie.py
@@ -1,2 +1,4 @@
-""" A text-based video game that acts as an interactive movie. 
+"""Lauren: A text-based video game that acts as an interactive movie. 
     Users will be able to make choices that affect the outcome of the story. """
+    
+# Emily: There will be two story lines and we might take information from files
