cb80242b817d28432fa25ec1cafd6a7d0257abed
MinghaoFang <81783977+MinghaoFang@users.noreply.github.com>
2021-04-01 22:11:16 -0400

    Update interactive_movie.py

--- interactive_movie.py
+++ interactive_movie.py
@@ -2,3 +2,6 @@
     Users will be able to make choices that affect the outcome of the story. """
     
 # Emily: There will be two story lines and we might take information from files
+
+"""Minghao Fang: We may need the user to provide some of their information such as their names
+and age in order to become a player in this game."
