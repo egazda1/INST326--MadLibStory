363ba79ffb9b9cbc1180af7972539a78aa0ebebf
Lauren Walsh <lwalsh52@terpmail.umd.edu>
2021-04-16 10:33:54 -0400

    main class

--- interactive_movie.py
+++ interactive_movie.py
@@ -8,6 +8,11 @@ and age in order to become a player in this game."
 
 Shirui Dong: Users will need to make decisions with the use of pictures as visuals of different choices/ objects/locations
 """
+class Main():
+    """
+    """
 class Player():
+    """ Gets information from user like name, age, gender
+    """
     
 if __name__ == "__main__":
