ef9f77255b1ea7eda9716ee56b6a85ce64c2fce0
Lauren Walsh <lwalsh52@terpmail.umd.edu>
2021-04-16 10:54:04 -0400

    classes

--- interactive_movie.py
+++ interactive_movie.py
@@ -9,10 +9,24 @@ and age in order to become a player in this game."
 Shirui Dong: Users will need to make decisions with the use of pictures as visuals of different choices/ objects/locations
 """
 class Main():
-    """
+    """ This is where the story occurs.
     """
 class Player():
-    """ Gets information from user like name, age, gender
-    """
+    """ Gets information from user 
     
+    Attributes:
+        name(string):
+        age(int):
+        gender(string):
+        hp(float):
+    """
+    def __init__():
+class Weapons():
+    """
+    """
+    def __init__():
+class Enemy():
+    """
+    """
+    def __init__():
 if __name__ == "__main__":
