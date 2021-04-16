c414b876fe255b48279be0fb6bd4a5110f610a56
ShiruiDong <sdong12@terpmail.umd.edu>
2021-04-16 19:44:10 -0400

    Update interactive_movie.py

--- interactive_movie.py
+++ interactive_movie.py
@@ -13,7 +13,6 @@ def main():
     """ This is where the story occurs. We will have the main script here.
         Side effect: printing story
     """
-    if __name__ == "__main__":
     
 class Player():
     """ Ask user to input their information, such as name, gender and age so they can become part of the game 
@@ -108,3 +107,5 @@ class Fight():
             Returns:
                 hp_of_enemy(int): The remaining hp of the enemy
         """
+if __name__ == "__main__":
+    main（）
